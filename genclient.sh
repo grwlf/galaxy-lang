13161423376a9f3d69f2be0a446dcc3a80f6eea7
Sergey Mironov <grrwlf@gmail.com>
2020-07-17 21:59:14 +0300

    Update scripts

--- genclient.sh
+++ genclient.sh
@@ -5,7 +5,7 @@ set -e -x
 
 openapi-generator-cli generate \
   -i https://icfpc2020-api.testkontur.ru/swagger/api/swagger.json \
-  -g python -o `pwd`/3rdparty
+  -g python -o `pwd`/3rdparty/openapi
 
 rm -rf `pwd`/src/openapi_client || true
-cp -r `pwd`/3rdparty/openapi_client `pwd`/src/openapi_client
+cp -r `pwd`/3rdparty/openapi/openapi_client `pwd`/src/openapi_client
