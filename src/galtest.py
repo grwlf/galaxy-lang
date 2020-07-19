fc313f69af3bdac07d68e6dbc2661fbf93820824
Sergey Mironov <grrwlf@gmail.com>
2020-07-19 15:47:38 +0300

    Mass update

--- src/galtest.py
+++ src/galtest.py
@@ -3,7 +3,21 @@ from os.path import join
 
 from solution.lang import *
 
-interp_test('eq', '''
+run_test('add', '''
+  #ap (ap ap add 1 1) 1 = 2
+  ap ap ap add 1 1 1 = 2
+
+  # ap (ap add (ap (ap add 1) 2)) 3  = 5
+  ap ap add ap ap add 1 2 3  = 6
+
+  ap ap add 1 2   =   3
+  ap ap add 2 1   =   3
+  ap ap add 0 1   =   1
+  ap ap add 2 3   =   5
+  ap ap add 3 5   =   8
+  ''')
+
+run_test('eq', '''
   ap ap eq 33 33   =   t
   ap ap eq 0 -2   =   f
   ap ap eq 0 -1   =   f
@@ -32,22 +46,14 @@ interp_test('eq', '''
   ap ap eq -21 -20   =   f
   ''')
 
-interp_test('add', '''
-    ap ap add 1 2   =   3
-    ap ap add 2 1   =   3
-    ap ap add 0 1   =   1
-    ap ap add 2 3   =   5
-    ap ap add 3 5   =   8
-    ''')
 
-
-interp_test('mul', '''
+run_test('mul', '''
     ap ap mul 4 2   =   8
     ap ap mul 3 4   =   12
     ap ap mul 3 -2   =   -6
     ''')
 
-interp_test('div', '''
+run_test('div', '''
   ap ap div 4 2   =   2
   ap ap div 4 3   =   1
   ap ap div 4 4   =   1
@@ -59,7 +65,7 @@ interp_test('div', '''
   ap ap div -5 -3   =   1
   ''')
 
-interp_test('neg', '''
+run_test('neg', '''
   ap neg 0   =   0
   ap neg 1   =   -1
   ap neg -1   =   1
@@ -67,138 +73,138 @@ interp_test('neg', '''
   ap neg -2   =   2
   ''')
 
-interp_test('ap', '''
-  ap inc ap inc 0   =   2
-  ap inc ap inc ap inc 0   =   3
-  ap ap add ap ap add 2 3 4   =   9
-  ap ap add 2 ap ap add 3 4   =   9
-  ap ap add ap ap mul 2 3 4   =   10
-  ap ap mul 2 ap ap add 3 4   =   14
-  ap inc ap dec 44   =   44
-  ap dec ap inc 33   =   33
-  ap dec ap ap add 33 1   =   33
-  ''')
+#run_test('ap', '''
+#  ap inc ap inc 0   =   2
+#  ap inc ap inc ap inc 0   =   3
+#  ap ap add ap ap add 2 3 4   =   9
+#  ap ap add 2 ap ap add 3 4   =   9
+#  ap ap add ap ap mul 2 3 4   =   10
+#  ap ap mul 2 ap ap add 3 4   =   14
+#  ap inc ap dec 44   =   44
+#  ap dec ap inc 33   =   33
+#  ap dec ap ap add 33 1   =   33
+#  ''')
 
-interp_test('c', '''
-  ap ap ap c add 1 2   =   3
-  ''')
+#run_test('c', '''
+#  ap ap ap c add 1 2   =   3
+#  ''')
 
-interp_test('s', '''
-  ap ap ap s add inc 1   =   3
-  ap ap ap s mul ap add 1 6   =   42
-  ''')
+#run_test('s', '''
+#  ap ap ap s add inc 1   =   3
+#  ap ap ap s mul ap add 1 6   =   42
+#  ''')
 
-interp_test('b', '''
-  ap ap ap b inc dec 33   =   33
-  ''')
+#run_test('b', '''
+#  ap ap ap b inc dec 33   =   33
+#  ''')
 
-# ap pwr2 0   =   ap ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 (3.1.2 . ap 3 (b 0.5 1 2))
-
-
-
-
-# FIXME: pwr2 requires untyped interpetation
-
-interp_test('pwr2', '''
-  # pwr2   =   ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1
-  ap pwr2 1   =   2
-
-  # ap pwr2 0   =   ap ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
-  # ap pwr2 0   =   ap ap ap ap c ap eq 0 1 0 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
-  # ap pwr2 0   =   ap ap ap ap eq 0 0 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
-  # ap pwr2 0   =   ap ap t 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
-  ap pwr2 0   =   1
-
-
-  # ap pwr2 1   =   ap ap mul 2 ap ap ap ap eq 0 ap ap add -1 1 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
-  # ap pwr2 1   =   ap ap mul 2 ap ap ap ap eq 0 0 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
-  # ap pwr2 1   =   ap ap mul 2 ap ap t 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
-  ap pwr2 1   =   ap ap mul 2 1
-  ''')
-
-interp_test('i', '''
-  ap i 343   =   343
-  ap i 1   =   1
-  # ap i i   =   i
-  # ap i add   =   add
-  # ap i ap add 1   =   ap add 1
-  ''')
-
-interp_test('car', '''
-  ap car ap ap cons 33 42   =   33
-  ap car ap ap cons 33 42   =   33
-  #ap car x2   =   ap x2 t
-  ''')
-
-interp_test('cdr', '''
-  ap cdr ap ap cons 33 nil   =   nil
-  ''')
-
-interp_test('cons', '''
-  #ap ap ap cons x0 x1 x2   =   ap ap x2 x0 x1
-  ap car ap ap cons 33 ap ap cons 42 ap ap cons 666 nil = 33
-  ''')
-
-# interp_test('nil', '''
-#   ap nil 33   =   t
-#   ''')
-
-interp_test('isnil', '''
-  ap isnil nil   =   t
-  ap isnil ap ap cons 33 44   =   f
-  ''')
-
-interp_test('if0', '''
-  ap ap ap if0 0 33 42   =   33
-  ap ap ap if0 1 33 42   =   42
-  ''')
-
-
-assert nbits_mod4(0)==0
-assert nbits_mod4(1)==1
-assert nbits_mod4(7)==1
-assert nbits_mod4(15)==1
-assert nbits_mod4(16)==2, f'{nbits_mod4(16)}'
-assert nbits_mod4(255)==2
-assert nbits_mod4(256)==3
-
-
-def modem_test(x:Val):
-  print(f"Testing modem on {x}")
-  s=mod_val(x).body
-  x2,sz=dem_val(s)
-  assert x==x2, f"{x} != {x2}"
-  assert sz==len(s)
-
-modem_test(mknil())
-modem_test(mkint(0))
-modem_test(mkint(-1))
-modem_test(mkint(-257))
-modem_test(mkint(1024))
-modem_test(mktuple(mkint(0),mknil()))
-modem_test(mktuple(mkint(1),mktuple(mkint(2),mktuple(mkint(3),mknil()))))
-modem_test(mktuple(mkint(1),mkint(2)))
-
-
-V=run_program(':33', interp_program('''
-  :0 = cons
-  :1 = 22
-  :2 = 33
-  :33 = ap ap :0 :1 :2
-  '''))
-print(V)
+## ap pwr2 0   =   ap ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 (3.1.2 . ap 3 (b 0.5 1 2))
 
 
 
 
+## FIXME: pwr2 requires untyped interpetation
 
-G=open(join(environ['SOLUTION_SOURCE'],'galaxy.txt'),'r').read()
-S=interp_program(G)
-S=interp_program('''
-  :9999 = ap ap galaxy nil ap ap cons 0 0
-''',S)
+#run_test('pwr2', '''
+#  # pwr2   =   ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1
+#  ap pwr2 1   =   2
+
+#  # ap pwr2 0   =   ap ap ap s ap ap c ap eq 0 1 ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
+#  # ap pwr2 0   =   ap ap ap ap c ap eq 0 1 0 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
+#  # ap pwr2 0   =   ap ap ap ap eq 0 0 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
+#  # ap pwr2 0   =   ap ap t 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 0
+#  ap pwr2 0   =   1
 
 
-# print(P)
+#  # ap pwr2 1   =   ap ap mul 2 ap ap ap ap eq 0 ap ap add -1 1 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
+#  # ap pwr2 1   =   ap ap mul 2 ap ap ap ap eq 0 0 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
+#  # ap pwr2 1   =   ap ap mul 2 ap ap t 1 ap ap ap b ap mul 2 ap ap b pwr2 ap add -1 ap ap add -1 1
+#  ap pwr2 1   =   ap ap mul 2 1
+#  ''')
+
+#run_test('i', '''
+#  ap i 343   =   343
+#  ap i 1   =   1
+#  # ap i i   =   i
+#  # ap i add   =   add
+#  # ap i ap add 1   =   ap add 1
+#  ''')
+
+#run_test('car', '''
+#  ap car ap ap cons 33 42   =   33
+#  ap car ap ap cons 33 42   =   33
+#  #ap car x2   =   ap x2 t
+#  ''')
+
+#run_test('cdr', '''
+#  ap cdr ap ap cons 33 nil   =   nil
+#  ''')
+
+#run_test('cons', '''
+#  #ap ap ap cons x0 x1 x2   =   ap ap x2 x0 x1
+#  ap car ap ap cons 33 ap ap cons 42 ap ap cons 666 nil = 33
+#  ''')
+
+## run_test('nil', '''
+##   ap nil 33   =   t
+##   ''')
+
+#run_test('isnil', '''
+#  ap isnil nil   =   t
+#  ap isnil ap ap cons 33 44   =   f
+#  ''')
+
+#run_test('if0', '''
+#  ap ap ap if0 0 33 42   =   33
+#  ap ap ap if0 1 33 42   =   42
+#  ''')
+
+
+#assert nbits_mod4(0)==0
+#assert nbits_mod4(1)==1
+#assert nbits_mod4(7)==1
+#assert nbits_mod4(15)==1
+#assert nbits_mod4(16)==2, f'{nbits_mod4(16)}'
+#assert nbits_mod4(255)==2
+#assert nbits_mod4(256)==3
+
+
+#def modem_test(x:Val):
+#  print(f"Testing modem on {x}")
+#  s=mod_val(x).body
+#  x2,sz=dem_val(s)
+#  assert x==x2, f"{x} != {x2}"
+#  assert sz==len(s)
+
+#modem_test(mknil())
+#modem_test(mkint(0))
+#modem_test(mkint(-1))
+#modem_test(mkint(-257))
+#modem_test(mkint(1024))
+#modem_test(mktuple(mkint(0),mknil()))
+#modem_test(mktuple(mkint(1),mktuple(mkint(2),mktuple(mkint(3),mknil()))))
+#modem_test(mktuple(mkint(1),mkint(2)))
+
+
+#V=run_program(':33', interp_program('''
+#  :0 = cons
+#  :1 = 22
+#  :2 = 33
+#  :33 = ap ap :0 :1 :2
+#  '''))
+#print(V)
+
+
+
+
+
+#G=open(join(environ['SOLUTION_SOURCE'],'galaxy.txt'),'r').read()
+#S=interp_program(G)
+#S=interp_program('''
+#  :9999 = ap ap galaxy nil ap ap cons 0 0
+#''',S)
+
+
+## print(P)
 
 
--- src/solution/lang.py
+++ src/solution/lang.py
@@ -11,87 +11,92 @@ from ipdb import set_trace
 # |_|   \__,_|_|  |___/\___|
 
 
-OPS:Dict[str,'Op']={}
+TERMS:Dict[str,'Term']={}
 collect_ops:bool=True
 
 @dataclass(frozen=True, eq=True)
-class Op:
+class Term:
   name:str
   nargs:int
 
   def __post_init__(self):
     if collect_ops:
-      global OPS
-      assert self.name not in OPS
-      OPS[self.name]=self
+      global TERMS
+      assert self.name not in TERMS
+      TERMS[self.name]=self
 
-T=Op('t',0)
-F=Op('f',0)
-inc=Op('inc',1)
-dec=Op('dec',1)
-add=Op('add',2)
-mul=Op('mul',2)
-div=Op('div',2)
-eq=Op('eq',2)
-lt=Op('lt',2)
-mod=Op('mod',1) # has a list version
-dem=Op('dem',1)
-send=Op('send',1)
-neg=Op('neg',1)
-ap=Op('ap',2)
-S=Op('s',3)
-C=Op('c',3)
-B=Op('b',3)
-pwr2=Op('pwr2',2)
-I=Op('i',1)
-cons=Op('cons',2)
-vec=Op('vec',2)
-car=Op('car',2)
-cdr=Op('cdr',2)
-nil=Op('nil',1)
-isnil=Op('isnil',1)
-draw=Op('draw',1) # [x,y]->img
-checkerboard=Op('checkerboard',2)
-multipledraw=Op('multipledraw',1) # [x]->img???
-if0=Op('if0',3)
+T=Term('t',0)
+F=Term('f',0)
+nil=Term('nil',0)
+inc=Term('inc',1)
+dec=Term('dec',1)
+add=Term('add',2)
+mul=Term('mul',2)
+div=Term('div',2)
+eq=Term('eq',2)
+lt=Term('lt',2)
+mod=Term('mod',1) # has a list version
+dem=Term('dem',1)
+send=Term('send',1)
+neg=Term('neg',1)
+ap=Term('ap',2)
+S=Term('s',3)
+C=Term('c',3)
+B=Term('b',3)
+pwr2=Term('pwr2',2)
+I=Term('i',1)
+cons=Term('cons',2)
+vec=Term('vec',2)
+car=Term('car',2)
+cdr=Term('cdr',2)
+isnil=Term('isnil',1)
+draw=Term('draw',1) # [x,y]->img
+checkerboard=Term('checkerboard',2)
+multipledraw=Term('multipledraw',1) # [x]->img???
+if0=Term('if0',3)
 collect_ops=False
 
-def isop(s:str)->bool:
-  return s in set(OPS.keys())
+def isterm(s:str)->bool:
+  return s in set(TERMS.keys())
 
-def mkop(s:str)->Op:
-  return OPS[s]
+def mkterm(s:str)->Term:
+  return TERMS[s]
 
 from enum import Enum
 class TType(Enum):
   """ Token types"""
   Int=0
   List=2
-  Op=3
+  Term=3
   Ref=4
 
-@dataclass
-class Picture:
-  data:List[Tuple[int,int]]
+# @dataclass
+# class Picture:
+#   data:List[Tuple[int,int]]
 
 @dataclass(frozen=True)
 class Ref:
   to:str
 
 def isref(s:str)->bool:
-  if s=='galaxy':
-    return True
-  else:
-    return s[0]==':' and str(int(s[1:]))==s[1:]
+  return len(s)>0 and s[0].isalpha() and (s not in TERMS)
 
 def mkref(s:str)->Ref:
   assert isref(s)
   return Ref(s)
 
+REFIDX:int=0
+
+def newref(suffix:str='x')->Ref:
+  global REFIDX
+  ref=mkref(f"{suffix}{REFIDX}"); REFIDX+=1
+  return ref
+
+
 @dataclass
 class Token:
   typ:TType
-  val:Union[int,list,Op,Ref]
+  val:Union[int,list,Term,Ref]
 
 def isint(w:str)->bool:
   try:
@@ -119,8 +124,8 @@ def parse_expr(line:str)->List[Token]:
       acc.append(Token(TType.Int, int(w)))
     elif isref(w):
       acc.append(Token(TType.Ref, mkref(w)))
-    elif isop(w):
-      acc.append(Token(TType.Op, mkop(w)))
+    elif isterm(w):
+      acc.append(Token(TType.Term, mkterm(w)))
     else:
       raise ValueError(f'Unknown expr token "{w}"')
   return acc
@@ -161,21 +166,19 @@ def parse_program(src:str)->Program:
 
 class VType(Enum):
   Int=0
-  Pic=1
-  Tuple=2
-  TLam=3
-  Bool=4
-  TErr=5
-  Nil=6
-  Thunk=7
-  Ref=8
-
-ValUnion=Union[int,Picture,Tuple['Val','Val'],'Lam',bool,'Err','Nil','Thunk',Ref]
+  Tuple=1
+  Lam=2
+  Bool=3
+  Err=4
+  Nil=5
+  Ap=6
+  Ref=7
+  Op=8
 
 @dataclass(frozen=True)
 class Val:
   typ:VType
-  val:ValUnion
+  val:'ValUnion'
 
 @dataclass(frozen=True)
 class Err:
@@ -186,21 +189,25 @@ class Nil:
   pass
 
 @dataclass(frozen=True)
-class Thunk:
+class Ap:
   f:Val
   x:Val
 
+@dataclass(frozen=True)
 class Lam:
-  def __init__(self, fn:Callable[[Val],Val]):
-    self.fn:Callable[[Val],Val]=fn
-
+  pat:Ref
+  body:Val
 
 @dataclass
-class State:
-  mem:Dict[Ref,Val]
+class Op:
+  term:Term
+  bind:List[Ref]
 
+ValUnion=Union[int,Tuple[Val,Val],Lam,bool,Err,Nil,Ap,Ref,Op]
 
-STATE:State=State({})
+def mklam(body:Callable[[Ref],Val])->Val:
+  ref=newref()
+  return Val(VType.Lam, Lam(ref,body(ref)))
 
 def asint(v:Val)->int:
   assert isinstance(v,Val)
@@ -208,30 +215,13 @@ def asint(v:Val)->int:
   assert isinstance(v.val,int), f"{v.val}"
   return int(v.val)
 
-def force(v:Val)->Val:
-  assert isinstance(v,Val), f"{v}"
-  while True:
-    if v.typ==VType.Thunk:
-      assert isinstance(v.val,Thunk)
-      v=call(v.val.f,v.val.x)
-    elif v.typ==VType.Ref:
-      assert isinstance(v.val,Ref)
-      v=STATE.mem[v.val]
-    else:
-      return v
-
-def call(f:Val, v:Val)->Val:
-  assert isinstance(f,Val) and isinstance(v,Val)
-  f=force(f)
-  v=force(v)
-  assert f.typ==VType.TLam, f"{f}"
-  assert isinstance(f.val,Lam)
-  return f.val.fn(v)
-
 Stack=List[Val]
 
 def mkint(i:int)->Val:
   return Val(VType.Int, i)
+
+def mkbool(b:bool)->Val:
+  return Val(VType.Bool, b)
 
 def mktuple(a:Val,b:Val)->Val:
   return Val(VType.Tuple, (a,b))
@@ -239,26 +229,52 @@ def mktuple(a:Val,b:Val)->Val:
 def mknil()->Val:
   return Val(VType.Nil, Nil())
 
-def mkstate0()->State:
-  return State({})
+def mkop(t:Term, binds:List[Ref])->Val:
+  return Val(VType.Op, Op(t,binds))
+
+def mkap(f:Val, x:Val)->Val:
+  return Val(VType.Ap, Ap(f,x))
+
+
+def pval(v:Val)->str:
+  if v.typ==VType.Int:
+    return str(v.val)
+  elif v.typ==VType.Tuple:
+    return f"({pval(v.val[0])},{pval(v.val[1])})"
+  elif v.typ==VType.Lam:
+    return f"({v.val.pat.to} -> {pval(v.val.body)})"
+  elif v.typ==VType.Bool:
+    return 't' if bool(v.val) else 'f'
+  elif v.typ==VType.Err:
+    return 'E'
+  elif v.typ==VType.Nil:
+    return 'nil'
+  elif v.typ==VType.Ap:
+    return f"({pval(v.val.f)} {pval(v.val.x)})"
+  elif v.typ==VType.Ref:
+    return v.val.to
+  elif v.typ==VType.Op:
+    return f"{v.val.term.name} {' '.join([r.to for r in v.val.bind])}"
+  else:
+    assert False, f"{v}"
+
 
 def checkerr(vals, lam)->Val:
   """ DEPRECATED"""
   for v in vals:
     assert isinstance(v, Val)
-    if v.typ==VType.TErr:
+    if v.typ==VType.Err:
       return v
   return lam(*vals)
 
-def chkerr1(lam:Callable[[Val],Val])->Callable[[Val],Val]:
-  def _lam(v:Val)->Val:
-    assert isinstance(v, Val)
-    if v.typ==VType.TErr:
-      return v
-    else:
-      return lam(v)
-  return _lam
-
+# def chkerr1(lam:Callable[[Ref],Val])->Callable[[Ref],Val]:
+#   def _lam(v:Val)->Val:
+#     assert isinstance(v, Val)
+#     if v.typ==VType.Err:
+#       return v
+#     else:
+#       return lam(v)
+#   return _lam
 
 def div_c0(a, b):
   if (a >= 0) != (b >= 0) and a % b:
@@ -266,201 +282,228 @@ def div_c0(a, b):
   else:
     return a // b
 
-
-_ERR = lambda msg : Val(VType.TErr, msg)
+_ERR = lambda msg : Val(VType.Err, msg)
 
 # FIXME: typed interpretation may reject some programs, like pwr2
 _T = Val(VType.Bool, True)
 _F = Val(VType.Bool, False)
-# _T = Val(VType.TLam, Lam(lambda a: Val(VType.TLam, Lam(lambda b: a))))
-# _F = Val(VType.TLam, Lam(lambda a: Val(VType.TLam, Lam(lambda b: b))))
+# _T = Val(VType.Lam, mklam(lambda a: Val(VType.Lam, mklam(lambda b: a))))
+# _F = Val(VType.Lam, mklam(lambda a: Val(VType.Lam, mklam(lambda b: b))))
 
-def interp_expr(expr:List[Token], s:State)->Stack:
+def load_expr(expr:List[Token])->Val:
+  def _lam1(op):
+    return mklam(lambda a: mkop(op,[a]))
+  def _lam2(op):
+    return mklam(lambda a: mklam(lambda b: mkop(op,[a,b])))
+  def _lam3(op):
+    return mklam(lambda a: mklam(lambda b: mklam(lambda c: mkop(op,[a,b,c]))))
   stack:Stack=[]
   for t in reversed(expr):
-    assert isinstance(t,Token), f"{t}"
+    assert isinstance(t,Token)
     if t.typ==TType.Int:
       assert isinstance(t.val, int)
       stack.append(Val(VType.Int, t.val))
     elif t.typ==TType.Ref:
       assert isinstance(t.val, Ref)
       stack.append(Val(VType.Ref, t.val))
-    elif t.typ==TType.Op:
-      assert isinstance(t.val, Op)
+    elif t.typ==TType.Term:
+      assert isinstance(t.val, Term)
       if t.val==T:
         stack.append(_T)
       elif t.val==F:
         stack.append(_F)
-      elif t.val==eq:
-        def _eq(a,b):
-          assert a.typ==b.typ
-          if a.val==b.val:
-            return _T
-          else:
-            return _F
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a: Val(VType.TLam,
-                         Lam(lambda b: _eq(a,b))))))
-      elif t.val==lt:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a: Val(VType.TLam,
-                         Lam(lambda b:
-                           checkerr([a,b], lambda a,b:
-                             Val(VType.Bool, asint(a)<asint(b))))))))
-      elif t.val==mul:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a: Val(VType.TLam,
-                         Lam(lambda b:
-                           checkerr([a,b], lambda a,b:
-                             Val(VType.Int,asint(a)*asint(b))))))))
-      elif t.val==add:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a: Val(VType.TLam,
-                         Lam(lambda b: Val(VType.Int, asint(a)+asint(b)))))))
-      elif t.val==div:
-        def _div(a,b):
-          if b.typ==VType.Int and int(b.val)==0:
-            return _ERR(f"Division by zero")
-          return checkerr([a,b], lambda a,b: div_c0(asint(a),asint(b)))
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a: Val(VType.TLam,
-                         Lam(lambda b: Val(VType.Int,_div(a,b)))))))
-      elif t.val==neg:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a: Val(VType.Int, -asint(a)))))
-      elif t.val==inc:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a:
-                           checkerr([a], lambda a: Val(VType.Int, asint(a)+1)))))
-      elif t.val==dec:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a:
-                           checkerr([a], lambda a: Val(VType.Int,asint(a)-1)))))
-      elif t.val==pwr2:
-        def _pwr2(v):
-          if v.typ==VType.Int and int(v.val)<0:
-            return _ERR(f"pwr2: {v.val}<0")
-          else:
-            return Val(VType.Int, checkerr([v], lambda v: 2**asint(v)))
-        stack.append(Val(VType.TLam, Lam(_pwr2)))
-      elif t.val==C:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda a: Val(VType.TLam,
-                         Lam(lambda b: Val(VType.TLam,
-                         Lam(lambda c:
-                           call(call(a,c),b)
-                         )))))))
-      elif t.val==S:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda x0: Val(VType.TLam,
-                         Lam(lambda x1: Val(VType.TLam,
-                         Lam(lambda x2:
-                           call(call(x0,x2),call(x1,x2))
-                         )))))))
-      elif t.val==B:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda x0: Val(VType.TLam,
-                         Lam(lambda x1: Val(VType.TLam,
-                         Lam(lambda x2:
-                           call(x0,call(x1,x2))
-                         )))))))
-      elif t.val==I:
-        stack.append(Val(VType.TLam,
-                         Lam(lambda x0: x0)))
       elif t.val==nil:
-        _e:List[Val]=[]
-        stack.append(Val(VType.Nil, Nil()))
-      elif t.val==cons or t.val==vec:
-        def _cons(x0,x1)->Val:
-          assert isinstance(x0,Val) and isinstance(x1,Val)
-          return Val(VType.Tuple, (x0,x1))
-        stack.append(Val(VType.TLam,
-                         Lam(lambda x0: Val(VType.TLam,
-                         Lam(lambda x1: checkerr([x0,x1], _cons))))))
-      elif t.val==car:
-        def _car(x0:Val)->Val:
-          assert isinstance(x0,Val)
-          assert x0.typ==VType.Tuple
-          assert isinstance(x0.val,tuple)
-          return x0.val[0]
-        stack.append(Val(VType.TLam, Lam(chkerr1(_car))))
-
-      elif t.val==cdr:
-        def _cdr(x0:Val)->Val:
-          assert isinstance(x0,Val)
-          assert x0.typ==VType.Tuple
-          assert isinstance(x0.val,tuple)
-          return x0.val[1]
-        stack.append(Val(VType.TLam, Lam(chkerr1(_cdr))))
-
-      elif t.val==isnil:
-        def _isnil(v:Val)->Val:
-          return _T if v.typ==VType.Nil else _F
-        stack.append(Val(VType.TLam, Lam(chkerr1(_isnil))))
-
-      elif t.val==if0:
-        # def _isnil(v):
-        #   if v.typ!=VType.List:
-        #     return _ERR(f"Expected list, but got {v.typ}")
-        #   assert isinstance(v.val,list)
-        #   return _T if len(v.val)==0 else _F
-        # stack.append(Val(VType.TLam, Lam(chkerr1(_isnil))))
-        def _if0(c:Val,t:Val,f:Val)->Val:
-          if c.typ!=VType.Int:
-            return _ERR(f"if0: expected int, got '{c}'")
-          assert isinstance(c.val,int)
-          return t if c.val==0 else f
-        stack.append(Val(VType.TLam,
-                         Lam(lambda x0: Val(VType.TLam,
-                         Lam(lambda x1: Val(VType.TLam,
-                         Lam(lambda x2: checkerr([x0,x1,x2], _if0)
-                         )))))))
-
+        stack.append(mknil())
       elif t.val==ap:
         f=stack.pop()
         x=stack.pop()
-        stack.append(Val(VType.Thunk, Thunk(f,x)))
+        stack.append(mkap(f,x))
       else:
-        raise ValueError(f"Unsupported token op '{t.val}'")
+        if t.val.nargs==1:
+          stack.append(_lam1(t.val))
+        elif t.val.nargs==2:
+          stack.append(_lam2(t.val))
+        elif t.val.nargs==3:
+          stack.append(_lam3(t.val))
+        else:
+          raise ValueError(f"Cant deal with {t}, Help!")
     else:
       raise ValueError(f"Unsupported token type '{t.typ}'")
-  return stack
+  assert len(stack)==1, f"{[pval(x) for x in stack]}"
+  return stack[0]
 
 
-def interp_program(src:str, s0_:Optional[State]=None)->State:
+
+
+@dataclass
+class Memory:
+  space:Dict[Ref,Val]
+
+def mkmem()->Memory:
+  return Memory({})
+
+STATE:Memory=Memory({})
+
+def pprog(p:dict)->str:
+  acc=[]
+  for ref,val in p.items():
+    acc.append(f"{ref.to} = {pval(val)}")
+  return '\n'.join(acc)
+
+def pmem(m:Memory)->str:
+  return pprog(m.space)
+
+def ppr(x:Any)->None:
+  if isinstance(x,Memory):
+    print(pmem(x))
+  elif isinstance(x,Val):
+    print(pval(x))
+  elif isinstance(x,dict):
+    print(pprog(x))
+  else:
+    assert False, f"Can't print {x}, Help!"
+
+def load_program(src:str, m:Memory):
+  """ Mutatas existing memory or builds a new one by adding new expressions """
   p=parse_program(src)
-  s:State=s0_ if s0_ is not None else mkstate0()
   for i,(ref,expr) in enumerate(p.body.items()):
-    stack=interp_expr(expr, s)
-    assert len(stack)==1, f"{stack}"
-    s.mem[ref]=stack[0]
-  return s
+    v=load_expr(expr)
+    m.space[ref]=v
 
 
-def run_program(name:str, s:State)->Val:
-  global STATE
-  STATE=s
-  return force(STATE.mem[mkref(name)])
+
+def interp(m:Memory, target:Ref)->Val:
+  heap:Dict[Ref,Val]={}
+  queue:List[Ref]=[]
+
+  def _getmem(r:Ref)->Val:
+    return heap.get(r) or m.space[r]
+
+  set_trace()
+
+  queue.append(target)
+  while len(queue)>0:
+    cur=queue.pop()
+    v=_getmem(cur)
+    if v.typ==VType.Ap:
+      branches:List[Val]=[]
+      matched:Dict[Ref,Val]=OrderedDict()
+      while v.typ==VType.Ap:
+        assert isinstance(v.val, Ap)
+        branches.append(v.val.x)
+        v=v.val.f
+      while v.typ==VType.Lam:
+        assert isinstance(v.val, Lam)
+        if len(branches)==0:
+          break
+        matched[v.val.pat]=branches.pop()
+        v=v.val.body
+
+      # Split current target in two
+      ref=newref('e')
+      v2=Val(VType.Ref, ref)
+      for b in reversed(branches):
+        v2=mkap(v2,b)
+      queue.append(cur)
+      heap[cur]=v2
+
+      # Our op `v` with matched patterns
+      queue.append(ref)
+      heap[ref]=v
+
+      # Arguments of `v`
+      for ref,val in matched.items():
+        queue.append(ref)
+        heap[ref]=val
+
+    elif v.typ==VType.Lam:
+      assert isinstance(v.val, Lam)
+      assert False
+    elif v.typ==VType.Op:
+      assert isinstance(v.val, Op)
+      o:Op=v.val
+      if o.term==neg:
+        a=_getmem(o.bind[0])
+        heap[cur]=mkint(-asint(a))
+      elif o.term==add:
+        a=_getmem(o.bind[0])
+        b=_getmem(o.bind[1])
+        heap[cur]=mkint(asint(a)+asint(b))
+      elif o.term==mul:
+        a=_getmem(o.bind[0])
+        b=_getmem(o.bind[1])
+        heap[cur]=mkint(asint(a)*asint(b))
+      elif o.term==div:
+        a=_getmem(o.bind[0])
+        b=_getmem(o.bind[1])
+        heap[cur]=mkint(div_c0(asint(a),asint(b)))
+      elif o.term==eq:
+        a=_getmem(o.bind[0])
+        b=_getmem(o.bind[1])
+        if a.typ==b.typ:
+          heap[cur]=_T if a.val==b.val else _F
+        else:
+          heap[cur]=_F
+      else:
+        raise ValueError(f"Can't op {o}, Help!")
+    elif v.typ==VType.Int or v.typ==VType.Bool:
+      pass
+    elif v.typ==VType.Ref:
+      heap[cur]=_getmem(v.val)
+    else:
+      raise ValueError(f"Can't value {v}, Help!")
+
+  return _getmem(target)
+
+def interp_expr(v:Val)->Val:
+  m=Memory({mkref('tgt'):v})
+  return interp(m,mkref('tgt'))
 
 
-# def interp(p:str, s0=None)->State:
+# def interp(v:Val)->Val:
+#   assert isinstance(v,Val), f"{v}"
+#   while True:
+#     if v.typ==VType.Ap:
+#       assert isinstance(v.val,Ap)
+#       v=call(v.val.f,v.val.x)
+#     elif v.typ==VType.Ref:
+#       assert isinstance(v.val,Ref)
+#       v=STATE.mem[v.val]
+#     else:
+#       return v
+
+# def call(f:Val, v:Val)->Val:
+#   assert isinstance(f,Val) and isinstance(v,Val)
+#   f=force(f)
+#   v=force(v)
+#   assert f.typ==VType.Lam, f"{f}"
+#   assert isinstance(f.val,Lam)
+#   return f.val.fn(v)
+
+
+
+# def run_program(name:str, s:Memory)->Val:
+#   global STATE
+#   STATE=s
+#   return interp(STATE.mem[mkref(name)])
+
+
+# def interp(p:str, s0=None)->Memory:
 #   return interp_program(parse_program(p), s0)
 
-def interp_test(hint:str, test:str)->None:
+def run_test(hint:str, test:str)->None:
   print(f"Testing {hint}")
   for i,tl in enumerate(test.split('\n')):
     if len(tl.strip())==0 or tl.strip()[0]=='#':
       continue
     print(f"Checking line {i:02d}: {tl.strip()}")
     expr_test,expr_ans=tl.split('=')
-    stack_test=interp_expr(parse_expr(expr_test),mkstate0())
-    stack_ans=interp_expr(parse_expr(expr_ans),mkstate0())
-    assert len(stack_test)==1, f"len({stack_test})=={len(stack_test)}"
-    assert len(stack_ans)==1, f"{stack_ans}"
-    val_test=force(stack_test[0])
-    val_ans=force(stack_ans[0])
-    # assert val_test is not val_ans, f"{val_test}, {val_ans}"
-    assert val_test==val_ans, f"{val_test} != {val_ans}"
+    val_test=load_expr(parse_expr(expr_test))
+    val_ans=load_expr(parse_expr(expr_ans))
+
+    redex_test=interp_expr(val_test)
+    redex_ans=interp_expr(val_ans)
+    assert redex_test==redex_ans, f"{redex_test} != {redex_ans}"
 
 
 
