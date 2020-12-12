a0f92b52ee21061c26fe2001e6658bd5559b95d2
Sergey Mironov <grrwlf@gmail.com>
2020-12-13 01:29:40 +0300

    Fix pytest warnings

--- src/galang/sertlv.py
+++ src/galang/sertlv.py
@@ -6,7 +6,7 @@ from galang.interp import IMem, IExpr, IAp, ILam, IError, IVal, IMem
 
 
 import galang.sertlv_pb2 as pb
-from galang.sertlv_pb2 import Node, Value, Tuple, List as PBList
+from galang.sertlv_pb2 import Node, Value, Tuple as PBTuple, List as PBList
 
 from typing import List, Any
 
@@ -24,7 +24,7 @@ def _node(tag, value):
 def _tuple(v1,v2):
   assert isinstance(v1,Value)
   assert isinstance(v2,Value)
-  t=Tuple()
+  t=PBTuple()
   t.v1.CopyFrom(v1)
   t.v2.CopyFrom(v2)
   return t
@@ -59,23 +59,23 @@ def _value(v):
 def expr2tlv(e:Expr)->TLV:
   if isinstance(e, Val):
     if isinstance(e.val, Const):
-      return _node(Node.val, _value(_node(Node.const, _value(e.val.const))))
+      return _node(pb.val, _value(_node(pb.const, _value(e.val.const))))
     elif isinstance(e.val, Ref):
-      return _node(Node.val, _value(_node(Node.ref, _value(e.val.name))))
+      return _node(pb.val, _value(_node(pb.ref, _value(e.val.name))))
     else:
       raise ValueError(f"Invalid value expression {e}")
   elif isinstance(e, Lam):
-    return _node(Node.lam, _value((_value(e.name),
+    return _node(pb.lam, _value((_value(e.name),
                                    _value(expr2tlv(e.body)))))
   elif isinstance(e, Let):
-    return _node(Node.let, _value((_value(e.ref.name),
+    return _node(pb.let, _value((_value(e.ref.name),
                                    _value((_value(expr2tlv(e.expr)),
                                            _value(expr2tlv(e.body)))))))
   elif isinstance(e, Ap):
-    return _node(Node.ap, _value((_value(expr2tlv(e.func)),
+    return _node(pb.ap, _value((_value(expr2tlv(e.func)),
                                   _value(expr2tlv(e.arg)))))
   elif isinstance(e, Intrin):
-    return _node(Node.intrin, _value((_value(e.name.val),
+    return _node(pb.intrin, _value((_value(e.name.val),
                                       _value([_value((_value(n),
                                                       _value(expr2tlv(a))))
                                              for n,a in e.args.dict.items()]))))
@@ -84,26 +84,26 @@ def expr2tlv(e:Expr)->TLV:
 
 def tlv2expr(j:TLV)->Expr:
   typ = j.tag
-  if typ == Node.val:
+  if typ == pb.val:
     vtyp = j.value.node.tag
-    if vtyp == Node.const:
+    if vtyp == pb.const:
       return num(j.value.node.value.int64)
-    elif vtyp == Node.ref:
+    elif vtyp == pb.ref:
       return ref(j.value.node.value.string)
     else:
       raise ValueError(f"Invalid value expression {_flat(j)}")
-  elif typ==Node.lam:
+  elif typ==pb.lam:
     return lam(j.value.tuple.v1.string, lambda _: tlv2expr(j.value.tuple.v2.node))
-  elif typ==Node.let:
+  elif typ==pb.let:
     return let_(j.value.tuple.v1.string,
                           tlv2expr(j.value.tuple.v2.tuple.v1.node),
                 lambda _: tlv2expr(j.value.tuple.v2.tuple.v2.node))
-  elif typ==Node.ap:
+  elif typ==pb.ap:
     return ap(tlv2expr(j.value.tuple.v1.node),
               tlv2expr(j.value.tuple.v2.node))
-  elif typ==Node.intrin:
+  elif typ==pb.intrin:
     return intrin(MethodName(j.value.tuple.v1.string),
-                           [(v.tuple.v1.string,
+                  [(         v.tuple.v1.string,
                     tlv2expr(v.tuple.v2.node)) for v in j.value.tuple.v2.list.list])
   else:
     raise ValueError(f"Invalid expression {_flat(j)}")
